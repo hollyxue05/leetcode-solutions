063184dc3ad40d1777167d2816d432491b3ddb96
Holly Xue <140566479+hollyxue05@users.noreply.github.com>
2025-06-06 22:00:57 -0400

    Create 9-PalindromeNumber

---
+++ 9-PalindromeNumber/9-PalindromeNumber.java
@@ -0,0 +1,31 @@
+// Last updated: 6/6/2025, 10:00:56 PM
+import java.util.LinkedList;
+import java.util.Stack;
+import java.util.Queue;
+
+class Solution {
+    public boolean isPalindrome(int x) {
+        String s = String.valueOf(x);
+
+        Stack<Character> stack = new Stack<>();
+        Queue<Character> queue = new LinkedList<>();
+
+        int c1, c2;
+
+        for(int i = 0; i < s.length(); i++) {
+            stack.push(s.charAt(i));
+            queue.add(s.charAt(i));
+        }
+
+        while(!queue.isEmpty()) {
+            c1 = stack.pop();
+            c2 = queue.remove(); 
+            if(!(c1==c2))   return false;
+
+
+        }
+
+
+        return true;
+    }
+}
