fb72a51f1bdec97a999ad2b842d095a81ae60577
Holly Xue <140566479+hollyxue05@users.noreply.github.com>
2025-06-07 21:56:26 -0400

    File updated at 6/7/2025, 9:56:26 PM

--- 121-BestTimetoBuyandSellStock/121-BestTimetoBuyandSellStock.java
+++ 121-BestTimetoBuyandSellStock/121-BestTimetoBuyandSellStock.java
@@ -1,12 +1,11 @@
-// Last updated: 6/7/2025, 9:54:36 PM
+// Last updated: 6/7/2025, 9:56:26 PM
 class Solution {
     public int maxProfit(int[] prices) {
-        int maxProfit = 0; int profit = 0; int minCost = Integer.MAX_VALUE; int maxSell = 0; 
+        int maxProfit = 0; 
+        int minCost = Integer.MAX_VALUE;  
 
         for(int i = 0; i < prices.length; i++) {
             if(prices[i] < minCost)     minCost = prices[i]; 
-
-            if(prices[i] > maxSell)    maxSell = prices[i]; 
 
             maxProfit = Math.max(maxProfit, prices[i] - minCost);
         }
