c55b574af0eba34fc1825eea5cd558b25b1a3888
Holly Xue <140566479+hollyxue05@users.noreply.github.com>
2025-06-10 21:28:02 -0400

    File updated at 6/10/2025, 9:28:03 PM

--- 56-MergeIntervals/56-MergeIntervals.java
+++ 56-MergeIntervals/56-MergeIntervals.java
@@ -1,10 +1,11 @@
-// Last updated: 6/10/2025, 9:24:11 PM
+// Last updated: 6/10/2025, 9:28:03 PM
 class Solution {
     public int[][] merge(int[][] intervals) {
         Arrays.sort(intervals, (i1, i2) -> Integer.compare(i1[0], i2[0])); 
         
         List<int[]> result = new ArrayList<>();
 		int[] newInterval = intervals[0];
+        
 		result.add(newInterval);
 		for (int[] interval : intervals) {
 			if (interval[0] <= newInterval[1]) 
