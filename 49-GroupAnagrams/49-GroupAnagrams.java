6371ac1e0519385331f06ac62e604454e0221f1b
Holly Xue <140566479+hollyxue05@users.noreply.github.com>
2025-06-10 21:42:04 -0400

    Create 49-GroupAnagrams

---
+++ 49-GroupAnagrams/49-GroupAnagrams.java
@@ -0,0 +1,26 @@
+// Last updated: 6/10/2025, 9:42:05 PM
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, Integer> mp = new HashMap<>();
+        List<List<String>> result  = new ArrayList<>();
+
+        char[] chars; 
+        String sortedStr; 
+
+        for(String str : strs) {    
+            chars = str.toCharArray();       
+            Arrays.sort(chars);     
+            sortedStr = new String(chars);   
+            
+            if(mp.containsKey(sortedStr)) 
+                result.get(mp.get(sortedStr)).add(str);
+            
+            else {
+                mp.put(sortedStr, result.size());
+                result.add(new ArrayList<>(Arrays.asList(str)));
+            }
+        }   
+
+        return result; 
+    }
+}
