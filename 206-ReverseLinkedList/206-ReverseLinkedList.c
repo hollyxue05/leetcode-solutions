1d8c7e80e18178b889af976e66040d6b0884d025
Holly Xue <140566479+hollyxue05@users.noreply.github.com>
2025-06-14 12:35:51 -0400

    Create 206-ReverseLinkedList

---
+++ 206-ReverseLinkedList/206-ReverseLinkedList.c
@@ -0,0 +1,27 @@
+// Last updated: 6/14/2025, 12:35:53 PM
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+struct ListNode* reverseList(struct ListNode* head) {
+    if(head == NULL || head -> next == NULL)    return head; 
+
+    struct ListNode* prev = head; 
+    struct ListNode* curr = prev -> next;
+
+    head -> next = NULL; 
+
+    while(prev != NULL && curr != NULL) {
+        struct ListNode* next = curr -> next;
+        curr -> next = prev;
+
+        prev = curr; 
+        curr = next;
+    }
+
+    return prev; 
+}
