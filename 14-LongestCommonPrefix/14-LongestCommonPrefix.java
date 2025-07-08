3afd343c5f44bfec05c3dede72ab2cfe3777422b
Holly Xue <140566479+hollyxue05@users.noreply.github.com>
2025-07-08 16:00:27 -0400

    File updated at 7/8/2025, 4:00:27 PM

--- 14-LongestCommonPrefix/14-LongestCommonPrefix.java
+++ 14-LongestCommonPrefix/14-LongestCommonPrefix.java
@@ -1,39 +1,21 @@
-// Last updated: 7/8/2025, 3:48:47 PM
+// Last updated: 7/8/2025, 4:00:27 PM
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        String result = ""; 
-        int maxLength = Integer.MAX_VALUE; 
-        boolean prefix = true; 
+        String result  = ""; 
 
-        if(strs.length == 1)
-            return strs[0]; 
+        Arrays.sort(strs); 
+        String first = strs[0]; 
+        String last = strs[strs.length - 1];
 
-        for(int i = 0; i < strs.length; i++) {
-            maxLength = Math.min(strs[i].length(), maxLength);
+        System.out.println(first + ", " + last);
+
+        int index = 0; 
+        while(index < first.length() && index < last.length()) {
+            if(first.charAt(index) != last.charAt(index)) 
+                break; 
+            index++; 
         }
 
-        if(maxLength == 0) 
-            return result; 
-
-        for(int j = 0; j <= maxLength; j++) {
-            
-            // "abc", "abcd"    maxLength = 3
-            
-            for(int i = 1; i < strs.length; i++) {
-                String compare = strs[0].substring(0, j); 
-
-                System.out.println(compare + "    " + strs[i].substring(0, j));
-
-                if(!strs[i].substring(0, j).equals(compare)) {
-                    prefix = false; 
-                    return result; 
-                }
-
-                if(prefix && i == strs.length - 1)
-                    result = compare; 
-            }
-        }
-
-        return result; 
+        return first.substring(0, index); 
     }
 }
